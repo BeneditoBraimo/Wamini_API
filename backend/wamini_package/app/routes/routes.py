27de56ac25d165334a5fa473052537d54b232215
BeneditoBraimo <beneditobraimo@gmail.com>
2025-11-04 13:55:16 +0200

    added documentation for the routes.py script

---
+++ backend/wamini_package/app/routes/routes.py
@@ -0,0 +1,11 @@
+"""
+routes.py
+-------------
+This module defines the REST API routes for Wamini platform.
+
+Features:
+    - JWT-based athentication (for user signup/login)
+    - CRDU operations for User, Products, Inputs, Transports, Negotiations
+    - Modular structure using Flask Blueprints
+"""
+
