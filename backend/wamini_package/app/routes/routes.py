143dff58fa56b1540df39fc6b46c4f2c5e4a3ddd
BeneditoBraimo <beneditobraimo@gmail.com>
2025-11-04 14:04:10 +0200

    imported Blueprint and flask_jwt modules to the routes.py script

--- backend/wamini_package/app/routes/routes.py
+++ backend/wamini_package/app/routes/routes.py
@@ -9,3 +9,5 @@ Features:
     - Modular structure using Flask Blueprints
 """
 
+from flask import Blueprint, request, jsonify
+from flask import (create_access_token, jwt_required, get_jwt_identity)
