7809652ca222fb66d760a5af02a66af249bd32e9
BeneditoBraimo <beneditobraimo@gmail.com>
2025-11-07 22:43:18 +0200

    added code to load configurations from environment variables

--- backend/wamini_package/app/routes/__init__.py
+++ backend/wamini_package/app/routes/__init__.py
@@ -4,10 +4,13 @@ __init__.py
 
 initializes and registers all route blueprints for Wamini backend API.
 """
-
+import os
 from flask import Flask
 from flask_cors import CORS
 
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+from ..models import db
 # import blueprints from routes.py
 
 from .routes import (
@@ -26,10 +29,21 @@ def create_app():
             app (Flask): configured Flask application instance.
     """
 
+    # Load environment variables from backend/wamini_package/.env
+    env_path = os.path.join(os.path.dirname(__file__), '..', '..', '.env')
+    load_dotenv(env_path)
+
     app = Flask(__name__)
     CORS(app)
 
-    # Resister Blueprints with URL prefixes already defined in routes.py
+    # Basic configurations
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLAALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    
+
+    # Register Blueprints with URL prefixes already defined in routes.py
     app.register_blueprint(user_bp)
     app.register_blueprint(product_bp)
     app.register_blueprint(input_bp)
